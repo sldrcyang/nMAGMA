4d3b4f202856de6b374a61b6777fa8d8a6d9a73b
sldrcyang <67252954+sldrcyang@users.noreply.github.com>
2020-08-18 20:26:14 +0800

    Update Extended LDSC to GSA.R

--- Extended LDSC to GSA.R
+++ Extended LDSC to GSA.R
@@ -1,6 +1,6 @@
 setwd("/your path/")
 g=read.table("your .genes.out file", header = T,sep = "\t",stringsAsFactors = F)
-gs=readLines("your gene set annot file") #with each row corresponding to a gene set:name of the gene set followed by the gene IDs
+gs=readLines("your gene set annot file") 
 matrix=data.frame(g$GENE,stringsAsFactors = F)
 
 library(dplyr)
@@ -8,7 +8,6 @@ geneset <- c()
 genelist <- list()
 generows <- list()
 
-#Create a gene-geneset matrix
 for(i in 1:length(gs)){
   temp <- unlist(strsplit(gs[i], " "))
   geneset[i] <- temp[1]
@@ -22,17 +21,14 @@ for(i in 1:length(gs)){
   }
 }
 
-#remove gene sets which have no genes in .genes.out file 
 m.vars=apply(matrix,2,sum)
 matrix=matrix[,which(m.vars>0)] 
 
-#Scale each column of the matrix
 matrix.scale=matrix
 for (j in 1:ncol(matrix)){
   matrix.scale[,j]=scale(matrix[,j],center=T,scale=T) 
 }
 
-#Calculate the LD score of each gene set
 ld=c()
 for(i in 1:ncol(matrix.scale)){
   ld[i]=0
@@ -44,7 +40,6 @@ for(i in 1:ncol(matrix.scale)){
   }
 }
 
-#Calculate the chisq^2-Statistics of each gene set
 x <- scale(g$ZSTAT,center=T,scale=T)
 chi <- c()
 for(i in 1:ncol(matrix.scale)){
@@ -52,7 +47,6 @@ for(i in 1:ncol(matrix.scale)){
 }
 f1=data.frame(ld=ld,chi=chi,stringsAsFactors = FALSE)
 
-#Calculate the regression weights
 w=c()
 for(i in 1:length(ld)){
   w[i]=1/max(ld[i],1)
@@ -62,6 +56,5 @@ data=data[!(is.na(data$chi)),]
 data=data[!(is.na(data$ld)),]
 data=data[-which(data$chi>150),]  #Remove extreme values
 
-#Regression
-lm.reg <- lm((data$chi)~(data$ld),weights = data$w)  #加权最小二乘法
+lm.reg <- lm((data$chi)~(data$ld),weights = data$w)
 lm.reg$coefficients
